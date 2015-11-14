a565ced562fea23923883b70e2073a1fd35d1d7e
Konrad Neneman <konradneneman@gmail.com>
2015-11-14 18:25:51 +0100

    better driver for m25px32

--- inc/m25px32.h
+++ inc/m25px32.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+#include "stdbool.h"
 #define	M25_WRITE_ENABLE		0x06
 #define	M25_WRITE_DISABLE		0x04
 #define M25_READ_STATUS_REGISTER	0x05
@@ -17,25 +19,17 @@
 
 
 
-typedef struct flash_state{
-	unsigned char cell_index;
-	unsigned int page_index;
-} flash_state, * flash_state_p;
+
 
 void m25_init(void);
 void m25_prepare_to_start(void);
-void m25_save_byte(unsigned char byte);
-void m25_save_6bytes(unsigned char* bytes);
+bool m25_save_byte(unsigned char byte);
+bool m25_save_6bytes(unsigned char* bytes);
 void m25_load_all_data(void);
+bool m25_test(void);
 
 
-unsigned char m25_read_byte(void);
-//void m25_write_enable(void);
-void m25_start_writing_page(unsigned int addr);
-void m25_write_byte(unsigned char data);
-unsigned char m25_get_status(void);
-void m25_send_command(unsigned char comm);
-void m25_choose_address(unsigned long addr);
+
+//void m25_choose_address(unsigned long addr);
 void m25_erase_chip(void);
-flash_state_p m25_get_state(void);
 
--- src/m25px32.c
+++ src/m25px32.c
@@ -1,6 +1,7 @@
 #include "m25px32.h"
 #include "spi.h"
 #include "uart.h"
+
 #include <util/delay.h>
 
 #define RECEIVE 0x00
@@ -17,12 +18,22 @@
 #define wait_for_spi_ready() while(!spi_rdy)
 #define spi_busy()	spi_rdy=0
 
+static uint8_t m25_read_byte(void);
+static uint8_t m25_get_status(void);
+static void m25_send_command(unsigned char comm);
+
 extern volatile unsigned char spi_rdy;
 volatile unsigned char spi_received_value;
+
+typedef struct flash_state{
+	uint32_t flash_adress;
+	bool page_enabled;
+} flash_state, * flash_state_p;
 
 flash_state m25_state;
 //inicjalizujemy pamiec flash, czyscimy i zaczynami pisac od poczatku
 void m25_init(void){
+	m25_state.flash_adress=0;
 	SPI_init_master();
 	spi_rdy=1;
 	sei();
@@ -35,21 +46,21 @@ void m25_prepare_to_start(void){
 	m25_slave_select();
 	m25_send_command(M25_WRITE_ENABLE);
 	m25_send_command(M25_PAGE_PROGRAM);
-	m25_send_command((unsigned char)((m25_get_state()->page_index)>>8));
-	m25_send_command((unsigned char)(m25_get_state()->page_index));
-	m25_send_command(m25_get_state()->cell_index);
+	m25_send_command((uint8_t) (m25_state.flash_adress>>16));
+	m25_send_command((uint8_t) (m25_state.flash_adress>>8));
+	m25_send_command((uint8_t) m25_state.flash_adress);
 }
 
-void m25_save_byte(unsigned char byte)
+bool m25_save_byte(uint8_t byte)
 {
 	m25_send_command(byte);
-	if(++(m25_get_state()->cell_index)==0){
-		(m25_get_state()->page_index)++;
-		if (m25_get_state()->page_index>M25_MAX_PAGE_ADDRESS){
+	m25_state.flash_adress++;
+	if(m25_state.flash_adress%256==0){
+		if (m25_state.flash_adress>0x3FFFFF){
 			cbi(PORTC, 1); //zapalamy czerwona diode
 			wait_for_spi_ready();
 			m25_slave_free();
-			while(1);//skonczyla nam sie pamiec. Czekamy na reset
+			return false;
 		}
 		else {
 			wait_for_spi_ready();
@@ -58,22 +69,25 @@ void m25_save_byte(unsigned char byte)
 			m25_slave_select();
 			m25_send_command(M25_WRITE_ENABLE);
 			m25_send_command(M25_PAGE_PROGRAM);
-			m25_send_command((unsigned char)((m25_get_state()->page_index)>>8));
-			m25_send_command((unsigned char)(m25_get_state()->page_index));
-			m25_send_command(m25_get_state()->cell_index);
+			m25_send_command((uint8_t) (m25_state.flash_adress>>16));
+			m25_send_command((uint8_t) (m25_state.flash_adress>>8));
+			m25_send_command((uint8_t) m25_state.flash_adress);
 		}
 	}
+	return true;
 	
 
 }
 
-void m25_save_6bytes(unsigned char* bytes){
-	m25_save_byte(bytes[0]);
-	m25_save_byte(bytes[1]);
-	m25_save_byte(bytes[2]);
-	m25_save_byte(bytes[3]);
-	m25_save_byte(bytes[4]);
-	m25_save_byte(bytes[5]);
+bool m25_save_6bytes(unsigned char* bytes){
+	bool ret=true;
+	ret &= m25_save_byte(bytes[0]);
+	ret &= m25_save_byte(bytes[1]);
+	ret &= m25_save_byte(bytes[2]);
+	ret &= m25_save_byte(bytes[3]);
+	ret &= m25_save_byte(bytes[4]);
+	ret &= m25_save_byte(bytes[5]);
+	return ret;
 }
 
 void m25_load_all_data(void){
@@ -85,35 +99,40 @@ void m25_load_all_data(void){
 	m25_send_command(0x00);
 	m25_send_command(0x00);
 	m25_send_command(0x00);
-	for(i=0x1FFFFF; i<0x3FFFFF; ++i){
+	for(i=0/*0x1FFFFF*/; i<0x3FFFFF; ++i){//#TODO tutaj chyba od 0 jedziemy?
 		tmp = m25_read_byte();
 		UART_send_char(tmp);		
 	}
 }
 
-void m25_send_command(unsigned char comm){
+bool m25_test(void){
+	uint8_t tmp;
+	m25_slave_select();
+	m25_send_command(M25_READ_IDENTIFICATION1);
+	tmp=m25_read_byte();
+	if (tmp!=0x20)
+		return false;
+	tmp=m25_read_byte();
+	if (tmp!=0x71)
+		return false;
+	tmp=m25_read_byte();
+	if (tmp!=0x16)
+		return false;
+
+	return true;
+}
+
+static void m25_send_command(unsigned char comm){
 	wait_for_spi_ready();
 	SPI_send_char(comm);
 }
-void m25_choose_address(unsigned long addr){
-	wait_for_spi_ready();
-	SPI_send_char((unsigned char)(addr>>16));
-	wait_for_spi_ready();
-	SPI_send_char((unsigned char)(addr>>8));
-	wait_for_spi_ready();	
-	SPI_send_char((unsigned char)addr);
-}
-unsigned char m25_read_byte(void){
+static unsigned char m25_read_byte(void){
 	wait_for_spi_ready();
 	SPI_send_char(RECEIVE);
 	wait_for_spi_ready();
 	return spi_received_value;
 }
-void m25_write_byte(unsigned char data){
-	wait_for_spi_ready();
-	SPI_send_char(data);
-}
-unsigned char m25_get_status(void){
+static unsigned char m25_get_status(void){
 	m25_send_command(M25_READ_STATUS_REGISTER);
 	return m25_read_byte();
 }
@@ -123,7 +142,7 @@ void m25_erase_chip(void){
 	m25_slave_free();
 	asm("nop");
 	do{
-		_delay_ms(20);
+		_delay_ms(2000);
 		m25_slave_select();
 		tmp=m25_get_status();
 		m25_slave_free();
@@ -131,12 +150,4 @@ void m25_erase_chip(void){
 		
 	}while (tmp&0x01);
 }
-
-//------------------------------------PRIVATE--------------------------------------
-inline flash_state_p m25_get_state(void){
-	return &m25_state;
-}
-
-
-
 
